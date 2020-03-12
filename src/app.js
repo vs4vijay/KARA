498bd2af79d90c7072c1ebd58fd6bcdba6ca51a7
vs4vijay <vs4vijay@gmail.com>
2020-03-12 22:57:35 +0530

    Add redis dep. and docker compose file

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res, next) {
+app.get(['/', '/api'], function(req, res, next) {
   res.json({
     success: true,
     message: 'Server is Running'
